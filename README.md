3530e7143301fc11b8f7bd50f312c2d40151fb29
polirritmico <ebray187@gmail.com>
2025-05-06 02:23:27 -0400

    docs: update README

---
+++ README.md
@@ -0,0 +1,11 @@
+instructions:
+
+```bash
+cd user/oracle-instantclient
+abuild checksum
+abuild unpack
+abuild-keygen -a
+abuild -r
+
+cd ~/packages/user/x86_64
+```
--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-apk update || true
+# apk update || true
 
-cd /home/ci/aports
+# cd /home/ci/aports
 
 # generate and manually install rsa keys
-su ci sh -c "abuild-keygen -a -n"
-cp /home/ci/.abuild/ci-*.pub /etc/apk/keys/
+cd /home/ci/user/oracle-instantclient
+abuild checksum
+abuild unpack
+abuild-keygen -a -n
+abuild -r
 
-mkdir -p /home/ci/repos/alpine/user
-su root sh -c "chmod 777 -R /home/ci/repos/alpine/user"
-exec su ci sh -c "buildrepo -d /home/ci/repos/alpine user oracle-instantclient"
+# mkdir -p /home/ci/repos/alpine/user
+# su root sh -c "chmod 777 -R /home/ci/repos/alpine/user"
+# exec su ci sh -c "buildrepo -d /home/ci/repos/alpine user oracle-instantclient"
