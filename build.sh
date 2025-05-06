a3c666b156e907a29f495443ce5a0ca89332842a
polirritmico <ebray187@gmail.com>
2025-05-06 15:37:17 -0400

    feat: improve build script

--- build.sh
+++ build.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-# apk update || true
+cd /home/ci/aports/user/oracle-instantclient
 
-# cd /home/ci/aports
+MSG="[✔] Success! Packages built successfully in /home/ci/packages/user/x86_64/"
 
-# generate and manually install rsa keys
-cd /home/ci/user/oracle-instantclient
 abuild checksum
 abuild unpack
 abuild-keygen -a -n
-abuild -r
 
-# mkdir -p /home/ci/repos/alpine/user
-# su root sh -c "chmod 777 -R /home/ci/repos/alpine/user"
-# exec su ci sh -c "buildrepo -d /home/ci/repos/alpine user oracle-instantclient"
+abuild -r || echo "${MSG}"
