3d8eadcf4c300cfb37b530b46f3c4aa9547e5fd6
polirritmico <ebray187@gmail.com>
2025-05-07 10:34:02 -0400

    feat: update output message after the build process

--- packages_builder.sh
+++ packages_builder.sh
@@ -9,6 +9,6 @@ abuild-keygen -a -n
 
 abuild -r || {
     echo -e "\n-------------------------------------------------------------------------"
-    echo "✅ Success! Packages built successfully in /home/ci/packages/user/x86_64/"
+    echo "Check /home/ci/packages/user/x86_64/ for the generated packages."
     echo -e "Note: The 'UNTRUSTED signature' warning is expected for local builds.\nInstall packages with --allow-untrusted if needed."
 }
