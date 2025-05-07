a09d880dcf50db69d998ca85ab0da1b180f42101
polirritmico <ebray187@gmail.com>
2025-05-07 16:15:20 -0400

    feat: update packages_builder script

--- packages_builder.sh
+++ packages_builder.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+mkdir -p /home/ci/packages/user/x86_64
+
 cd /home/ci/aports/user/oracle-instantclient
+
 abuild checksum
 abuild unpack
 abuild-keygen -a -n
-
 abuild -r || {
+    cp /home/ci/packages/user/x86_64/* /home/ci/builds
     echo -e "\n-------------------------------------------------------------------------"
-    echo "Check /home/ci/packages/user/x86_64/ for the generated packages."
-    echo -e "Note: The 'UNTRUSTED signature' warning is expected for local builds.\nInstall packages with --allow-untrusted if needed."
+    echo -e "Success ✅. Generated packages exported into the ./build directory."
+    echo -e "NOTE: The 'UNTRUSTED signature' and 'Failed to create index' errors are"
+    echo -e "      expected in local builds."
+    echo -e "Install generated packages with --allow-untrusted if needed."
 }
