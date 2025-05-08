6d7f37e0a6877ef5060aa8806d70e08abacaba09
polirritmico <ebray187@gmail.com>
2025-05-07 21:15:40 -0400

    feat: change echo to printf to be POSIX compliant

--- packages_builder.sh
+++ packages_builder.sh
@@ -11,9 +11,9 @@ abuild unpack
 abuild-keygen -a -n
 abuild -r || {
     cp /home/ci/packages/user/x86_64/* /home/ci/builds
-    echo -e "\n-------------------------------------------------------------------------"
-    echo -e "Success ✅. Generated packages exported into the ./build directory."
-    echo -e "NOTE: The 'UNTRUSTED signature' and 'Failed to create index' errors are"
-    echo -e "      expected in local builds."
-    echo -e "Install generated packages with --allow-untrusted if needed."
+    printf "\n-------------------------------------------------------------------------\n"
+    printf "Success ✅.\nGenerated packages exported into the ./build directory.\n"
+    printf "NOTE: The 'UNTRUSTED signature' and 'Failed to create index' errors are\n"
+    printf "      expected in local builds.\n"
+    printf "Install generated packages with --allow-untrusted if needed.\n"
 }
